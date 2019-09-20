1faff3e5fb26a5fdf00cfa7a333d00285121c7b0
Danny Buckley <dannyboy.msn@hotmail.com>
2019-09-20 13:33:48 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,9 +2,16 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRAND = []
+
   def initialize(brand)
     @brand = brand
   end
+
+  def brand= (shoe_brand)
+    @brand = shoe_brand
+    BRAND << shoe_brand if BRAND.include?("shoe_brand")
+  end  
 
   def cobble
     self.condition = "new"
